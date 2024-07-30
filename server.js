5a48418e3319002d3e8f27fb9ae1b0381e4f52b8
EmekaRaphael <emekaraphael13@gmail.com>
2024-07-30 03:40:20 +0100

    init commit

--- server.js
+++ server.js
@@ -3,13 +3,6 @@ dotenv.config();
 import mongoose from "mongoose";
 import { app } from "./app.js";
 
-process.on('uncaughtException', err => {
-    console.log("UNCAUGHT EXCEPTION, Shutting down!");
-    console.log(err.name, err.message);
-    server.close(() => {
-        process.exit(1);
-    });
-});
 
 const DB = process.env.DATABASE.replace("<password>", process.env.DATABASE_PASSWORD);
 
@@ -20,6 +13,14 @@ mongoose
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
+});
+
+process.on('uncaughtException', err => {
+    console.log("UNCAUGHT EXCEPTION, Shutting down!");
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
 });
 
 process.on('unhandledRejection', err => {
