06fb13ec7334828cb4c5b9f69e73f156cc140e21
EmekaRaphael <emekaraphael13@gmail.com>
2024-09-03 08:17:11 +0100

    edited server.js file

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ dotenv.config();
 import mongoose from "mongoose";
 import { app } from "./app.js";
 
+// process.on('uncaughtException', err => {
+//     console.log("UNCAUGHT EXCEPTION, Shutting down!");
+//     console.log(err.name, err.message);
+//     server.close(() => {
+//         process.exit(1);
+//     });
+// });
 process.on('uncaughtException', err => {
-    console.log("UNCAUGHT EXCEPTION, Shutting down!");
-    console.log(err.name, err.message);
-    server.close(() => {
-        process.exit(1);
-    });
+    console.error("UNCAUGHT EXCEPTION! Shutting down...");
+    console.error(err.name, err.message, err.stack);
+    process.exit(1); 
 });
 
 const DB = process.env.DATABASE.replace("<password>", process.env.DATABASE_PASSWORD);
@@ -22,17 +27,24 @@ const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
 
+// process.on('unhandledRejection', err => {
+//     console.log("UNHANDLED REJECTION, Shutting down!");
+//     console.log(err.name, err.message);
+//     server.close(() => {
+//         process.exit(1);
+//     });
+// });
 process.on('unhandledRejection', err => {
-    console.log("UNHANDLED REJECTION, Shutting down!");
-    console.log(err.name, err.message);
+    console.error("UNHANDLED REJECTION! Shutting down...");
+    console.error(err.name, err.message, err.stack);
     server.close(() => {
         process.exit(1);
     });
 });
 
-// process.on("SIGTERM", () => {
-//     console.log("SIGTERM RECEIVED. Shutting down gracefully");
-//     server.close(() => {
-//         console.log("Process terminated!");
-//     });
-// });
+process.on("SIGTERM", () => {
+    console.log("SIGTERM RECEIVED. Shutting down gracefully");
+    server.close(() => {
+        console.log("Process terminated!");
+    });
+});
