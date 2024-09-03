e10c28fb5cfbe96648b6664efe6e6673c948ebb0
EmekaRaphael <emekaraphael13@gmail.com>
2024-09-03 02:43:17 +0100

    finally got stripe checkout page to work

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -14,7 +14,7 @@ export const bookTour = async tourId => {
 
         // 1) Get checkout session from API
         const session = await axios(
-            `http://127.0.0.1:8000/api/v1/bookings/checkout-session/${tourId}`
+            `http://localhost:8000/api/v1/bookings/checkout-session/${tourId}`
         );
                 
         // 2) Create checkout form + charge credit card
